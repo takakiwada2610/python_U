81a166c23f6f7eadeeb1dda55d34b790d7617e43
wada <tkkwd0526@yahoo.co.jp>
2022-11-03 00:05:06 +0900

    developでの８−９７コミット

--- siricon/section8.py
+++ siricon/section8.py
@@ -6,9 +6,9 @@ with open('test.csv','w') as csv_file:
     writer.writeheader()
     writer.writerow({'Name': 'A', 'Count': '1'})
     writer.writerow({'Name': 'B', 'Count': '2'})
-
+"""
 with open('test.csv', 'r') as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
         print(row['Name'], row['Count'])  
-        
+"""
