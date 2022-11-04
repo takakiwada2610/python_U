f3ffc29a57ecf5b1e9583f1149ff50f9d4fe65b0
wada <tkkwd0526@yahoo.co.jp>
2022-11-05 00:33:16 +0900

    セクション10-113のコミット1105

--- siricon/section8.py
+++ siricon/section8.py
@@ -1,14 +1,22 @@
-import csv
+import datetime
 
-with open('test.csv','w') as csv_file:
-    fieldnames = ['Name','Count']
-    writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerow({'Name': 'A', 'Count': '1'})
-    writer.writerow({'Name': 'B', 'Count': '2'})
-    
-with open('test.csv', 'r') as csv_file:
-    reader = csv.DictReader(csv_file)
-    for row in reader:
-        print(row['Name'], row['Count'])  
+now = datetime.datetime.now()
+print(now)
+print(now.isoformat())
+print(now.strftime('%d/%m/%y'))
 
+today = datetime.time(hour=1, minute=10, second=5, microsecond=100)
+print(today)
+
+print(now)
+# d =datetime.timedelta(weeks=1)
+d =datetime.timedelta(days=365)
+
+print(now - d)
+
+import time
+# print('###')
+# time.sleep(2)
+# print('###')
+
+print(time.time())
