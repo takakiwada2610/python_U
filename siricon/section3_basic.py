4f3bb55e591ef9612578eca8d727ab284b4b8af1
wada <tkkwd0526@yahoo.co.jp>
2022-11-09 16:13:55 +0900

    section3２週目変更

--- siricon/section3_basic.py
+++ siricon/section3_basic.py
@@ -1,6 +1,30 @@
-x = 10
+"""
+s = 'My name is Mike. Hi Mike'
+print(s)
 
-if x <  0:
-    print('negative')
-elif x == 0:
-    print('zero')
+is_start = s.startswith('s')
+print(is_start)
+
+print('###############')
+
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+print(s.count('Mike'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Mike', 'Nancy'))
+
+print('a is {} '.format('test'))
+print('a is {2} {1} {0} '.format(1, 2, 3))
+
+print('My name is {name} {family}. Watasi ha {family} {name}'.format(name='Jun', family='Sakai'))
+"""
+
+a = 'asad'
+print(f'a is {a}')
+
+name = 'Jun'
+family= 'Sakai'
+print(f'My name is {name} {family}. Watashi ha {family} {name}')
